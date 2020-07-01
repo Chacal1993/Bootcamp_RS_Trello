3dca7c08ef71884642d78da1f6adbbefc2e8c4d7
Chacal1993 <ale.gonzalez2@gmail.com>
2020-07-01 20:31:17 +0200

    refactor logic

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,137 +1,180 @@
-document.getElementById("modal").style.display = "none";
+/* referencia al contenedor padre de las tareas */
+let PADRE_SELECCIONADO = null;
+let TAREA_SELECCIONADA = null;
 
+function showModal(elementClicked) {
+    TAREA_SELECCIONADA = elementClicked;
+    /* obtener elemento por el id */
+    let idModal = document.getElementById('modal');
+    /* cambiar estilo a un elemento */
+    idModal.style.display = 'block';
 
-function showModal(padre) {
-    document.getElementById('btn_guardar').setAttribute('onclick', 'guardarDatos()')
-    let modal = document.getElementById("modal");
-    cargarDatos(padre)
-    modal.style.display = "block"
+    /* obtener padre de un elemento */
+    let nodoPadre = elementClicked.parentNode;
+
+    /* donde estoy (card) */
+    let abu = nodoPadre.parentNode;
+
+    PADRE_SELECCIONADO = abu;
+
+    selectButtonStatus(abu);
+
+    let title = elementClicked.innerText;
+    idModal.querySelector('#title_mod').innerHTML = title;
+    idModal.querySelector('#input_title').value = title;
 
 }
-
-let PARENT = null;
-function showModalNewCard(elemento) {
-    PARENT = elemento.parentNode;
-    document.getElementById('btn_guardar').setAttribute('onclick', 'guardarNewCard()');
-    let modal = document.getElementById("modal");
-    modal.style.display = "block";
-
-    editarTarea();
-    limpiar()
-}
-
-function limpiar() {
-    let getElementsEditable = document.getElementsByClassName('editable');
-    for (const iterator of getElementsEditable) {
-        iterator.value = "";
-    }
-}
-
 
 function cerrarModal() {
-    let modal = document.getElementById("modal");
-    modal.style.display = "none";
-    resetEditar();
+    let idModal = document.getElementById('modal');
+    idModal.style.display = 'none';
+
+    /* reestablecer botones */
+    let btnTd = document.getElementById('toDo');
+    btnTd.style.display = 'block';
+    let btnWip = document.getElementById('toWork');
+    btnWip.style.display = 'block';
+    let btnDone = document.getElementById('done');
+    btnDone.style.display = 'block';
+
+
+    reset();
+
+    TAREA_SELECCIONADA = null;
+
+}
+
+/* seleccionar botones que se muestran de acuerdo al componente donde se encuentran */
+function selectButtonStatus(contenedor) {
+    if (contenedor.id === 'td') {
+        let btnTd = document.getElementById('toDo');
+        btnTd.style.display = 'none';
+    } else if (contenedor.id === 'wip') {
+        let btnWip = document.getElementById('toWork');
+        btnWip.style.display = 'none';
+    } else {
+        let btnDone = document.getElementById('done');
+        btnDone.style.display = 'none';
+    }
 }
 
 function editarTarea() {
-    let getTitleEditar = document.getElementsByClassName('title_active');
-    let getInputButtons = document.getElementsByClassName('inactive');
-    let getTextArea = document.getElementsByClassName('editable');
-
-    /* recoger el valor del textarea antes de editar*/
-    let getValorTextArea = document.getElementsByName('textArea')[0].value;
-
-    for (const iterator of getTextArea) {
-        iterator.disabled = false;
-    }
-    while (getTitleEditar[0]) {
-        getTitleEditar[0].classList.add('title_inactive')
-        getTitleEditar[0].classList.remove('title_active')
-    }
-    while (getInputButtons[0]) {
-        getInputButtons[0].classList.add('active')
-        getInputButtons[0].classList.remove('inactive')
-    }
+    mostrarOcultar('inactive', 'active', 'title_active', 'title_inactive');
+    /* hacer editable los textos */
+    editable(false);
 }
 
+function mostrarOcultar(inactive, active, title_act, title_inact) {
+    let elementosInactivos = document.getElementsByClassName(inactive);
+    while (elementosInactivos[0]) {
+        elementosInactivos[0].classList.add(active);
+        elementosInactivos[0].classList.remove(inactive);
+    }
+    /* selecciono los elementos con clase title-active */
+    let elementActives = document.getElementsByClassName(title_act);
+    while (elementActives[0]) {
+        elementActives[0].classList.add(title_inact);
+        elementActives[0].classList.remove(title_act);
+    }
 
-function resetEditar() {
-    let getTitleInactive = document.getElementsByClassName('title_inactive');
-    let getEditable = document.getElementsByClassName('editable');
-    let getActive = document.getElementsByClassName('active');
-
-    while (getTitleInactive[0]) {
-        getTitleInactive[0].classList.add('title_active');
-        getTitleInactive[0].classList.remove('title_inactive')
-    }
-    for (const iterator of getEditable) {
-        iterator.disabled = true;
-    }
-    while (getActive[0]) {
-        getActive[0].classList.add('inactive')
-        getActive[0].classList.remove('active')
-    }
 }
 
-let TITLE_SUB_CARD = null;
+function editable(valor) {
+    let elementEditable = document.getElementsByClassName('editable');
 
-function cargarDatos(padreContenedor) {
-    /* recibir contenedor del sub_card para obtener si primer hijo*/
-    TITLE_SUB_CARD = padreContenedor;
-    let getTitleCard = padreContenedor.firstElementChild.innerHTML;
-    /* actualizar el título del modal con el title de la sub_card */
-    document.getElementById('title_mod').innerHTML = getTitleCard;
-    document.getElementById('input_title').value = getTitleCard;
+    for (const iter of elementEditable) {
+        iter.disabled = valor; //poner en falso
+    }
+}
+/* volver funciones al estado inicial */
+function reset() {
+    mostrarOcultar('active', 'inactive', 'title_inactive', 'title_active');
+    editable(true);
 }
 
 function guardarDatos() {
-    let getTitleInput = document.getElementById('input_title').value;
-    document.getElementById('title_mod').innerHTML = getTitleInput;
+    /* caso que la tarea ya está creada */
+    if (TAREA_SELECCIONADA != null) {
+        let input = document.getElementById('input_title').value;
 
-    if (TITLE_SUB_CARD != null) {
-        TITLE_SUB_CARD.firstElementChild.innerHTML = getTitleInput;
+        let textarea = document.getElementsByTagName('textarea')[0].value;
+        /* actualizar valor en el DOM */
+        document.getElementById("title_mod").innerHTML = input;
+
+        /* actualizar el titulo de la tarea */
+        /* acceder al titulo de la tarea por el padre global */
+        PADRE_SELECCIONADO.querySelector('.card_task>h4').innerHTML = input;
+        reset();
+    } else {
+        console.log('guardando nueva')
+        guardarNewCard();
+    }
+
+}
+
+function moverTarea(idBoton) {
+    if (idBoton === 'toWork') {
+        let contendorDestino = document.getElementById('wip');
+
+        let cardDestino = contendorDestino.querySelector('.card > .sub_card');
+        cardDestino.append(TAREA_SELECCIONADA);
+    } else if (idBoton === 'toDo') {
+        let contendorDestino = document.getElementById('td');
+
+        let cardDestino = contendorDestino.querySelector('.card > .sub_card');
+        cardDestino.append(TAREA_SELECCIONADA);
+    } else {
+        let contendorDestino = document.getElementById('dn');
+
+        let cardDestino = contendorDestino.querySelector('.card > .sub_card');
+        cardDestino.append(TAREA_SELECCIONADA);
     }
     cerrarModal();
 }
 
+function eliminarTarea() {
+    /* obtengo el padre que contiene esa tarea y le indico que la elimine */
+    let padreTarea = TAREA_SELECCIONADA.parentNode;
+    padreTarea.removeChild(TAREA_SELECCIONADA);
+    cerrarModal();
+}
+
+function showModalNewCard(boton) {
+
+    limpiar();
+    PADRE_SELECCIONADO = boton.parentNode;
+    let modal = document.getElementById('modal');
+    modal.style.display = 'block';
+
+    editable(false);
+    mostrarOcultar('inactive', 'active', 'title_active', 'title_inactive');
+
+}
+
 function guardarNewCard() {
-    let abu = null;
-    let newTitle = document.getElementById('input_title').value;
-    if (PARENT != null) {
-        abu = PARENT.parentNode;
+    let input = document.getElementById('input_title').value;
 
-        let newSubCard = abu.querySelector('.card>.sub_card')
-        console.log(newSubCard)
-        let html = `<div class="card card_task" onclick="showModal(this)"> <h4>${newTitle}</h4> </div>`;
+    let textarea = document.getElementsByTagName('textarea')[0].value;
+    /* actualizar valor en el DOM */
+    document.getElementById("title_mod").innerHTML = input;
 
-        /* insertar texto plano antes de cerrar newSubCard */
-        newSubCard.insertAdjacentHTML('beforeend', html);
-        cerrarModal();
-    }
-}
+    /* crear la tarea a insertar */
+    let html = `<div class="card card_task" onclick="showModal(this)"><h4>${input}</h4> </div>`;
 
-function moverTarea(id) {
-    eliminarTarea(TITLE_SUB_CARD);
-    if (id == 'toWork') {
-        let getElementWork = document.getElementById('wip');
-        let containerElement = getElementWork.querySelector('.card>.sub_card')
-        /* insertar un Nodo */
-        containerElement.append(TITLE_SUB_CARD)
-        console.log(getElementWork);
-    } else if (id == 'done') {
-        let getElementDone = document.getElementById('dn');
-        let contenedor = getElementDone.querySelector('.card>.sub_card');
-        contenedor.append(TITLE_SUB_CARD)
-    } else {
-        let getElemetToDo = document.getElementById('td');
-        let contenedorToDo = getElemetToDo.querySelector('.card>.sub_card');
-        contenedorToDo.append(TITLE_SUB_CARD);
-    }
+    /* donde insertamos */
+    let padre = PADRE_SELECCIONADO.parentNode;
+    console.log(padre)
+    /* insertando en el contenedor de tarjeta al final */
+    let insertar = padre.querySelector('.card>.sub_card');
+    insertar.insertAdjacentHTML('beforeend', html);
+
+
+    cerrarModal();
 
 }
 
-function eliminarTarea(tarea) {
-    let padreTarea = tarea.parentNode;
-    padreTarea.removeChild(tarea)
+function limpiar() {
+    document.getElementById('input_title').value = '';
+
+    document.getElementsByTagName('textarea')[0].value = '';
 }
